b16cff0288bd47974f2cc0c3033755ca138a3148
YasevichAlexandr <114285174+YasevichAlexandr@users.noreply.github.com>
2022-11-09 22:09:56 +0300

    Update ColloqTest.java

--- ColloqTest.java
+++ ColloqTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 public class ColloqTest {
     @org.junit.Test
 
+
     public void isInt() {
     }
     @org.junit.Test
@@ -10,6 +11,15 @@ public class ColloqTest {
     }
 
     @org.junit.Test
-    public void main() {
+    public void main()
+    {
+        String[] str1 =  {"bla", "empty", "Yas"};
+        String[] str2 = {"3", "0", "5" };
+        String[] str = {"blablabla", "", "YasYasYasYasYas"};
+        String res;
+        for(int i = 0; i < 3; i++){
+            res = Colloq.result(str1[i], str2[i]);
+            assertEquals(res, str[i]);
+        }
     }
 }
